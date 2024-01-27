78c75deac6bd6a839db3aa42060f748230f19ba2
ChocoKnight <ktan30@calpoly.edu>
2024-01-27 14:36:04 -0800

    added post method to add new users

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -41,6 +41,11 @@ const findUserByName = (name) => {
 
 const findUserById = (id) => 
     users["users_list"].find((user) => user["id"] === id);
+
+const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+}
   
 app.use(express.json());
 
@@ -57,6 +62,12 @@ app.get("/users", (req, res) => {
     } else {
         res.send(users);
     }
+});
+
+app.post("/users", (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
 });
 
 app.get("/users/:id", (req, res) => {
