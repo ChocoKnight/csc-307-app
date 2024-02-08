2172033d8aa88e21f81a9bd2a78369233be92710
ChocoKnight <ktan30@calpoly.edu>
2024-02-07 20:03:52 -0800

    Added user and user services

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,38 +1,39 @@
 import express from "express"
 import cors from "cors";
+import userServices from "./user-services";
 
 const app = express();
 const port = 8000;
 
-const users = {
-    users_list: [
-        {
-            id: "xyz789",
-            name: "Charlie",
-            job: "Janitor"
-        },
-        {
-            id: "abc123",
-            name: "Mac",
-            job: "Bouncer"
-        },
-        {
-            id: "ppp222",
-            name: "Mac",
-            job: "Professor"
-        },
-        {
-            id: "yat999",
-            name: "Dee",
-            job: "Aspring actress"
-        },
-        {
-            id: "zap555",
-            name: "Dennis",
-            job: "Bartender"
-        }
-    ]
-};
+// const users = {
+//     users_list: [
+//         {
+//             id: "xyz789",
+//             name: "Charlie",
+//             job: "Janitor"
+//         },
+//         {
+//             id: "abc123",
+//             name: "Mac",
+//             job: "Bouncer"
+//         },
+//         {
+//             id: "ppp222",
+//             name: "Mac",
+//             job: "Professor"
+//         },
+//         {
+//             id: "yat999",
+//             name: "Dee",
+//             job: "Aspring actress"
+//         },
+//         {
+//             id: "zap555",
+//             name: "Dennis",
+//             job: "Bartender"
+//         }
+//     ]
+// };
 
 function randomUserID () {
     let userID = "";
